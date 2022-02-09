67b3365aad9e7f3def3110b992d8af2b47da4447
dragonitebrian1024 <98874919+dragonitebrian1024@users.noreply.github.com>
2022-02-09 11:35:31 +0800

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,11 +1,26 @@
-﻿#include "Game.h"
+#include "Game.h"
 #include <string>
+#include "texturemanager.h" 
+#include "gameObject.h"
+#include "SDL.h"
+#include "ecs.h"
+#include "components.h"
 
-SDL_Texture* ballTex;
+
+gameObject* ball;
+gameObject* racket1;
+gameObject* racket2;
+
+SDL_Renderer* Game::renderer = nullptr;
+
+Manager manager;
+auto& newBall(manager.addEntity());
+auto& newPlayer(manager.addEntity());
 
 Game::Game()
 {
 	isRunning = true;
+
 }
 Game::~Game()
 {
@@ -45,17 +60,42 @@ void Game::init(std::string title, int xpos, int ypos, int width, int height, bo
 	} else {
 		isRunning = false;
 	}
+
+	ball = new gameObject("ball.png", 340, 250, 75, 80);
+	racket1 = new gameObject("racket.png", -50, 130, 120, 200);
+	racket2 = new gameObject("racket.png", 745, 140, 75, 200);
+	SDL_Rect wall;
+	wall.x = racket1->xpos;
+	wall.y = racket1->ypos;
+	wall.w = racket1->width;
+	wall.h = racket1->height;
+	SDL_Rect wall2;
+	wall2.x = racket2->xpos;
+	wall2.y = racket2->ypos;
+	wall2.w = racket2->width;
+	wall2.h = racket2->height;
+
+	newBall.getComponent<BallComponent>();
+	newPlayer.getComponent<RacketComponent1>();
 }
 
 void Game::update()
 {
-
+	ball->Update();
+	racket1->Update();
+	racket2->Update();
+	manager.update();
 }
 
 void Game::render()
 {
+	//ball texture
 	SDL_RenderClear(renderer);
-
+	ball->Render();
+	//racket1 texture
+	racket1->Render();
+	//racket2 texture
+	racket2->Render();
 	SDL_RenderPresent(renderer);
 }
 
@@ -71,6 +111,8 @@ void Game::handleEvents()
 
 	default:
 		break;
+
+		const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
 	}
 }
 
@@ -78,6 +120,7 @@ void Game::clean()
 {
 	SDL_DestroyWindow(window);
 	SDL_DestroyRenderer(renderer);
+	
 	SDL_Quit();
 	std::cout << "game cleaned" << std::endl;
 }
